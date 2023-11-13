3ec0f2215fbaedc2f2bc28bed54fb337a0cbbc6f
natcobbinah <baby.hugo@yahoo.com>
2023-11-13 07:26:14 +0100

    graph bfs & dfs done, priority queues using heaps in progress

--- python_dsa - Dr Basant Agarwal et al/graphs_and_other_algos/bfs_graph.py
+++ python_dsa - Dr Basant Agarwal et al/graphs_and_other_algos/bfs_graph.py
@@ -1,13 +1,23 @@
 from collections import deque
 
+# using a dictionary to represent the undirected graph in 
+# the graphnetwork 2.png image, the (Adjacency list) representation is
+graph = {}
+graph['A'] = ['B','D','G']
+graph['B'] = ['E','F']
+graph['C'] = ['F','H']
+graph['D'] = ['A','F']
+graph['E'] = ['B','G']
+graph['F'] = ['B','C','D']
+graph['G'] = ['A','E']
+graph['H'] = ['C']
+
 # To use BFS to traverse this graph, we will make use of a queue
 # In the wors-case scenario, each vertex or node and edge will be traversed
 # thus the time complexity of BFS algorithm is O(|V| + |E|), where
 # |V| = number of vertices or nodes and |E| = number of edges in the graph
-
-
 def breadth_first_search(graph, root):
-    visited_vertices = list()
+    visited_vertices = []
     graph_queue = deque([root])
     visited_vertices.append(root)
     node = root
@@ -24,7 +34,7 @@ def breadth_first_search(graph, root):
                 visited_vertices.append(elem)
                 graph_queue.append(elem)
 
-        return visited_vertices
+    return visited_vertices
 
 
-# Te
+print(breadth_first_search(graph, 'A'))
--- python_dsa - Dr Basant Agarwal et al/graphs_and_other_algos/dfs_graph.py
+++ python_dsa - Dr Basant Agarwal et al/graphs_and_other_algos/dfs_graph.py
@@ -7,5 +7,33 @@ graph['S'] = ['C','G']
 graph['D'] = ['C']
 graph['G'] = ['S','F','H']
 graph['H'] = ['G','E']
+graph['E'] = ['C','H']
 graph['F'] = ['C','G']
 graph['C'] = ['D','S','E','F']
+
+def depth_first_search(graph,root):
+    visited_vertices = []
+    graph_stack = []
+
+    graph_stack.append(root)
+    node = root
+
+    while len(graph_stack) > 0:
+        if node not in visited_vertices:
+            visited_vertices.append(node)
+        adjacent_nodes = graph[node]
+        
+        if set(adjacent_nodes).issubset(set(visited_vertices)):
+            graph_stack.pop()
+            if len(graph_stack) > 0:
+                node = graph_stack[-1]
+            continue 
+        else:
+            remaining_elements = set(adjacent_nodes).difference(set(visited_vertices))
+
+            first_adjacent_node = sorted(remaining_elements)[0]
+            graph_stack.append(first_adjacent_node)
+            node = first_adjacent_node
+    return visited_vertices
+
+print(depth_first_search(graph,'A'))
