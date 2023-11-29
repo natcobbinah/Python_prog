d4ee99067c6c55941f0fec2e6f460ef38e61a5a6
natcobbinah <baby.hugo@yahoo.com>
2023-11-29 07:21:58 +0100

    bayer-moore algorithm impl and tested

---
+++ python_dsa - Dr Basant Agarwal et al/string_algos_and_techniques/test-reverseloop.py
@@ -0,0 +1,4 @@
+pattern = "acacac"
+
+for j in range(len(pattern) - 1, -1 , -1):
+    print(f"{pattern[j]} {j}")
---
+++ python_dsa - Dr Basant Agarwal et al/string_algos_and_techniques/test_rfind.py
@@ -0,0 +1,11 @@
+text = 'acbcabccababcaacbcac'
+pattern = 'acbcac'
+
+text_search = "bacbabababacaca"
+pattern_search = "ababaca"
+
+#rfind =  last occurence of a substring
+#find = first occurence of a substring
+
+print(text.rfind(pattern))
+print(text_search.rfind(pattern_search))
